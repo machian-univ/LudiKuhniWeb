0379df28a6b04d923a3135dafaaa721f6387a7e8
Maria <machian.sfedu@gmail.com>
2022-11-13 19:02:38 +0300

    Added database requests for orders

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,33 @@
 'use strict'
+const db = require('../settings/db');
 
-exports.getHomePage = (req, res) => {
-    res.send('Hello, user!')
+exports.getHomePage = async (req, res) => {
+
+    var orderList
+    
+    const userID = req.session.passport.user
+    let result = await db.awaitQuery("SELECT * FROM orders WHERE userID = ?", [userID])
+    if(result.length) {
+
+        orderList = await Promise.all(result.map(async (orderItem) => {
+    
+            var order = {
+            title: orderItem.title,
+            discription: orderItem.discription,
+            status: undefined
+            }
+            
+            const statusID = orderItem.statusID
+            let result = await db.awaitQuery("SELECT * FROM statuses WHERE id = ?", [statusID])
+            order.status = result[0].title
+    
+            return order
+        }))
+    
+        res.send(orderList)
+    } else {
+
+        res.send("Пока Ваш список заказов пуст!")
+    }
+    
 }
