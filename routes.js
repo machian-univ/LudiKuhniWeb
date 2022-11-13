720f68d675bbacaa963c572a0db928897762c25c
Maria <machian.sfedu@gmail.com>
2022-11-13 19:01:32 +0300

    Created validator_config.js

--- routes.js
+++ routes.js
@@ -1,17 +1,16 @@
 'use strict'
 
-const innerController =  require('./controllers/indexController')
+const indexController =  require('./controllers/indexController')
 const authController = require('./controllers/authController')
 const userController = require('./controllers/userController.js')
+const { SignUpValidate, SignInValidate } = require('./settings/validator_config')
 
-const { check } = require('express-validator');
-const { body } = require('express-validator');
 
 module.exports = (app, passport) => {
       
     app
         .route('/')
-        .get(innerController.getMainPage)
+        .get(indexController.getMainPage)
 
     app
         .route('/signup')
@@ -46,44 +45,6 @@ module.exports = (app, passport) => {
         .get(LogOut)
 }
 
-var SignUpValidate = [
-    body('username')
-        .escape()
-        .ltrim(' ')
-        .rtrim(' '),
-    body('name')
-        .escape()
-        .ltrim(' ')
-        .rtrim(' '),
-    body('password')
-        .escape(), 
-    check('username', 'Некорректный Email.')
-        .isEmail(),
-    check('name')
-        .isLength({ min: 3 }).withMessage("Имя должно содержать минимум 3 символа."),
-    check('password')
-        .isLength({ min: 6 }).withMessage('Пароль должен содеражать миниммум 6 символов.')
-        .isLength({ max: 20 }).withMessage('Пароль должен содеражать максимум 20 символов.')
-        .matches('[0-9]').withMessage('Пароль должен содержать минимум одну цифру.')
-        .matches('[a-zA-Z]').withMessage('Пароль должен содержать минимум одну латинскую букву.')
-        .isAlphanumeric().withMessage('Пароль должен состоять из цифр и латиских букв.')
-    ]
-
-var SignInValidate = [
-    body('username')
-        .escape()
-        .ltrim(' ')
-        .rtrim(' '),
-    body('password')
-        .escape(), 
-    check('username')
-        .isEmail(),
-    check('password')
-        .isLength({ min: 6, max: 20 })
-        .matches('[0-9]')
-        .matches('[a-zA-Z]')
-        .isAlphanumeric()
-];
 
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated())
---
+++ settings/validator_config.js
@@ -0,0 +1,42 @@
+const { check, body } = require('express-validator')
+
+var SignUpValidate = [
+    body('username')
+        .escape()
+        .ltrim(' ')
+        .rtrim(' '),
+    body('name')
+        .escape()
+        .ltrim(' ')
+        .rtrim(' '),
+    body('password')
+        .escape(), 
+    check('username', 'Некорректный Email.')
+        .isEmail(),
+    check('name')
+        .isLength({ min: 3 }).withMessage("Имя должно содержать минимум 3 символа."),
+    check('password')
+        .isLength({ min: 6 }).withMessage('Пароль должен содеражать миниммум 6 символов.')
+        .isLength({ max: 20 }).withMessage('Пароль должен содеражать максимум 20 символов.')
+        .matches('[0-9]').withMessage('Пароль должен содержать минимум одну цифру.')
+        .matches('[a-zA-Z]').withMessage('Пароль должен содержать минимум одну латинскую букву.')
+        .isAlphanumeric().withMessage('Пароль должен состоять из цифр и латиских букв.')
+    ]
+
+var SignInValidate = [
+    body('username')
+        .escape()
+        .ltrim(' ')
+        .rtrim(' '),
+    body('password')
+        .escape(), 
+    check('username')
+        .isEmail(),
+    check('password')
+        .isLength({ min: 6, max: 20 })
+        .matches('[0-9]')
+        .matches('[a-zA-Z]')
+        .isAlphanumeric()
+]
+
+module.exports = { SignUpValidate, SignInValidate }
