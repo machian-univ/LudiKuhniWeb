9aef88fb8f7513c289237ebd53cd91087ee85823
Maria <machian.sfedu@gmail.com>
2022-11-23 23:52:11 +0300

    Fixed orderList

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -4,14 +4,12 @@ const db = require('../settings/db');
 
 exports.getHomePage = async (req, res) => {
 
-    var orderList
-
     const username = req.user.name
     const userID = req.user.id
     let result = await db.awaitQuery("SELECT * FROM orders WHERE userID = ?", [userID])
     if(result.length) {
 
-        orderList = await Promise.all(result.map(async (orderItem) => {
+        var orderList = await Promise.all(result.map(async (orderItem) => {
 
             var order = {
             title: orderItem.title,
@@ -26,9 +24,12 @@ exports.getHomePage = async (req, res) => {
             return order
         }))
 
-        res.render('../Views/pug/user.pug', {orders: orderList, name_user: username})
-    } else {
+        result = orderList
+    } 
 
-        res.render('../Views/pug/user.pug', {message: "Пока Ваш список заказов пуст!", name_user: username})
-    }
+    res.render('../Views/pug/user.pug', {
+        orders: result, 
+        message: "Пока Ваш список заказов пуст!", 
+        name_user: username
+    })
 }
